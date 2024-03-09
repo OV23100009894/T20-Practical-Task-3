644a38dd8349217683762caca685e01792fc47a6
Oleg Volosyanko <ovolosyanko@yahoo.co.uk>
2024-03-09 10:51:34 +0100

    function added to add two input data

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,17 @@
-# T20 Practical Task 2
+# T20 Practical Task 3
 
-user_input = input("Please type any data:")
-print(user_input)
+a = input("Please enter number 1:")
+b = input("Please enter number 2:")
+
+def sum_fn(num1, num2):
+    if(a.isnumeric() and b.isnumeric()):
+        return str(int(a) + int(b))
+    else:
+        return "\033[31mInput data error\033[00m"
+
+result = sum_fn(a, b)
+if result.isnumeric():
+    print(f"The sum of two numbers is {result}")
+else:
+    print(result)
 
